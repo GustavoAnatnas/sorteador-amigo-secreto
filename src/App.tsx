db922906646c8320ad606f9afe1bd4b9c6e102e4
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-10-16 20:22:41 -0300

    feat: recoilroot e primeira rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Forms from "./components/Forms";
+
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <RecoilRoot>
+          <Routes>
+                <Route path='/' element={Forms}/>
+          </Routes>
+        </RecoilRoot>
+      </BrowserRouter>
     </div>
   );
 }
