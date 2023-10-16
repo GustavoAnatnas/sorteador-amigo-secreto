3b50f4d3b299158dcf13cbfbc87fe432b19f2b84
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-10-16 20:51:51 -0300

    feat: formulário funcional com testes ok

--- src/components/Forms.test.tsx
+++ src/components/Forms.test.tsx
@@ -1,12 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Forms from "./Forms";
+import { RecoilRoot } from "recoil";
 
 test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
-    render(<Forms/>)
+    render(<Forms />)
 
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
     const button = screen.getByRole('button')
 
     expect(input).toBeInTheDocument()
     expect(button).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Forms />
+        </RecoilRoot>
+    )
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const button = screen.getByRole('button')
+
+    fireEvent.change(input, {
+        target: {
+            value: 'Gustavo'
+        }
+    })
+    fireEvent.click(button)
+    expect(input).toHaveFocus()
+    expect(input).toHaveValue('')
 })
--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,12 +1,33 @@
+import { useRef, useState } from "react";
+import { useAddParticipant } from "../state/hook/useAddParticipant";
+
 const Forms = () => {
+    const [name, setName] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const addInList = useAddParticipant()
+
+    const addParticipant = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        addInList(name)
+        setName('')
+        inputRef.current?.focus()
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={addParticipant}
+        >
             <input
+                ref={inputRef}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 type="text"
                 placeholder="Insira os nomes dos participantes"
             />
             <button
-            disabled={true}
+                disabled={!name}
             >
                 Adicionar
             </button>
