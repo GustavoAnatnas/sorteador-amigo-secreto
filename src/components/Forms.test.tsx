cbfcdaf9fff801fe9c55cef50e51bdbc07f3a033
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-10-16 18:00:53 -0300

    test: quando input vazio, novos participantes podem ser adicionados

---
+++ src/components/Forms.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import React from React;
+
+test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+    render(<Forms/>)
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const button = screen.getByRole('button')
+
+    expect(input).toBeInTheDocument()
+    expect(button).toBeDisabled()
+})
