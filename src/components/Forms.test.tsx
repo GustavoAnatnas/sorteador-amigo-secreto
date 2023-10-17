17b1be5e9940c274ab1c921762f4b45a9fd67f29
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-10-17 20:29:27 -0300

    test: timer para sumir mensagem

--- src/components/Forms.test.tsx
+++ src/components/Forms.test.tsx
@@ -1,66 +1,106 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Forms from "./Forms";
 import { RecoilRoot } from "recoil";
+import { act } from "react-dom/test-utils";
 
-test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
-    render(
-        <RecoilRoot>
-            <Forms />
-        </RecoilRoot>
-    )
+describe('Componente formulário', () => {
+    test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+        render(
+            <RecoilRoot>
+                <Forms />
+            </RecoilRoot>
+        )
 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const button = screen.getByRole('button')
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const button = screen.getByRole('button')
 
-    expect(input).toBeInTheDocument()
-    expect(button).toBeDisabled()
-})
-
-test('adicionar um participante caso exista um nome preenchido', () => {
-    render(
-        <RecoilRoot>
-            <Forms />
-        </RecoilRoot>
-    )
-
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const button = screen.getByRole('button')
-
-    fireEvent.change(input, {
-        target: {
-            value: 'Gustavo'
-        }
+        expect(input).toBeInTheDocument()
+        expect(button).toBeDisabled()
     })
-    fireEvent.click(button)
-    expect(input).toHaveFocus()
-    expect(input).toHaveValue('')
-})
 
-test('nomes duplicados não podem ser adicionados na lista' ,() =>{
-    render(
-        <RecoilRoot>
-            <Forms />
-        </RecoilRoot>
-    )
+    test('adicionar um participante caso exista um nome preenchido', () => {
+        render(
+            <RecoilRoot>
+                <Forms />
+            </RecoilRoot>
+        )
 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const button = screen.getByRole('button')
-    fireEvent.change(input, {
-        target: {
-            value: 'Gustavo'
-        }
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const button = screen.getByRole('button')
+
+        fireEvent.change(input, {
+            target: {
+                value: 'Gustavo'
+            }
+        })
+        fireEvent.click(button)
+        expect(input).toHaveFocus()
+        expect(input).toHaveValue('')
     })
-    fireEvent.click(button)
 
-    fireEvent.change(input, {
-        target: {
-            value: 'Gustavo'
-        }
+    test('nomes duplicados não podem ser adicionados na lista', () => {
+        render(
+            <RecoilRoot>
+                <Forms />
+            </RecoilRoot>
+        )
+
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const button = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'Gustavo'
+            }
+        })
+        fireEvent.click(button)
+
+        fireEvent.change(input, {
+            target: {
+                value: 'Gustavo'
+            }
+        })
+        fireEvent.click(button)
+
+        const errorMessage = screen.getByRole('alert')
+        expect(errorMessage.textContent).toBe('Nomes duplicados não são permitidos!')
+
+
     })
-    fireEvent.click(button)
 
-    const errorMessage = screen.getByRole('alert')
-    expect(errorMessage.textContent).toBe('Nomes duplicados não são permitidos!')
+    test('a mensagem de erro deve sumir após o timer finalizar', () => {
+        jest.useFakeTimers()
+        render(
+            <RecoilRoot>
+                <Forms />
+            </RecoilRoot>
+        )
 
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const button = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'Gustavo'
+            }
+        })
+        fireEvent.click(button)
+
+        fireEvent.change(input, {
+            target: {
+                value: 'Gustavo'
+            }
+        })
+        fireEvent.click(button)
+
+        let errorMessage = screen.queryByRole('alert')
+        expect(errorMessage).toBeInTheDocument()
+
+        act(() => {
+            jest.runAllTimers()
+
+        })
+
+        errorMessage = screen.queryByRole('alert')
+        expect(errorMessage).toBeNull()
+    })
 
 })
