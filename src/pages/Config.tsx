3c32caacc779406ceab07ba3c031af0bd7af8439
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-10-19 16:29:32 -0300

    test: config

---
+++ src/pages/Config.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer"
+import Forms from "../components/Forms"
+import { ListParticipants } from "../components/ListParticipants"
+
+export const Config = () => {
+    return(
+        <>
+        <Forms/>
+        <ListParticipants />
+        <Footer />
+        </>
+    )
+}
